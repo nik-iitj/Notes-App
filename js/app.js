b137feedee272c0c241f0de277224952dd1c0c4b
nik-iitj <raj.14@iitj.ac.in>
2022-01-11 23:44:49 +0530

    modification

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 console.log("Welcome to notes app. This is app.js");
 showNotes();
 
+var x;
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
