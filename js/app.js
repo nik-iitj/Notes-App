05d6870e2a376bd0ae5f1c802309ffb935b9b94a
nik-iitj <raj.14@iitj.ac.in>
2022-01-12 00:16:11 +0530

    Edge cases Handled

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-console.log("Welcome to notes app. This is app.js");
+
 showNotes();
 
-var x;
-// If user adds a note, add it to the localStorage
+
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
@@ -10,7 +9,10 @@ addBtn.addEventListener("click", function (e) {
     let notes = localStorage.getItem("notes");
     let radioButtons = document.querySelectorAll('input[name="size"]');
 
-    let currTag;
+   
+
+
+    let currTag="";
 
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
@@ -18,28 +20,34 @@ addBtn.addEventListener("click", function (e) {
             break;
         }
     }
+    if(addTitle .value==="" || currTag==="") console.log("Can't leave these fields empty");
 
+    else {
 
-    if (notes == null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
+        if (notes == null) {
+            notesObj = [];
+        } else {
+            notesObj = JSON.parse(notes);
+        }
+        let myObj = {
+            title: addTitle.value,
+            text: addTxt.value,
+            tag: currTag
+        }
+        notesObj.push(myObj);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        addTxt.value = "";
+        addTitle.value = "";
+    
+        showNotes();
+
     }
-    let myObj = {
-        title: addTitle.value,
-        text: addTxt.value,
-        tag: currTag
-    }
-    notesObj.push(myObj);
-    localStorage.setItem("notes", JSON.stringify(notesObj));
-    addTxt.value = "";
-    addTitle.value = "";
 
-    //   console.log(notesObj);
-    showNotes();
+
+
 });
 
-// Function to show elements from localStorage
+
 function showNotes() {
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -63,13 +71,13 @@ function showNotes() {
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
     } else {
-        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+        notesElm.innerHTML = `Notes Empty! Use "Add Note" section to save your work :)`;
     }
 }
 
-// Function to delete a note
+
 function deleteNote(index) {
-    //   console.log("I am deleting", index);
+  
 
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -88,7 +96,7 @@ let search = document.getElementById('searchTxt');
 search.addEventListener("input", function () {
 
     let inputVal = search.value.toLowerCase();
-    // console.log('Input event fired!', inputVal);
+
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
@@ -110,7 +118,6 @@ function favTutorial() {
     let noteCards = document.getElementsByClassName('noteCard');
 
     
-    // document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;
     Array.from(noteCards).forEach(function (element) {
 
     
